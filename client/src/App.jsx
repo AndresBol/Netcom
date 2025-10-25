c7d1b8ce3fd84ec9dfb2e9ce4c309b889190a47a
Andres <andresbol1706@gmail.com>
2025-10-24 19:40:30 -0600

    Create the context for public access

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,17 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { Layout } from "./components/layout/Layout";
 import { appTheme } from "./themes/theme";
+import { UserProvider } from "./contexts/UserContext";
 
 export default function App() {
   return (
-    <ThemeProvider theme={appTheme}>
-      <CssBaseline enableColorScheme />
-      <Layout>
-        <Outlet />
-      </Layout>
-    </ThemeProvider>
+    <UserProvider>
+      <ThemeProvider theme={appTheme}>
+        <CssBaseline enableColorScheme />
+        <Layout>
+          <Outlet />
+        </Layout>
+      </ThemeProvider>
+    </UserProvider>
   );
 }
