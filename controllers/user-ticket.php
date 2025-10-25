59eb6e10350ca68a34beea285bf4c9e9109cfc14
Andres <andresbol1706@gmail.com>
2025-10-25 16:45:46 -0600

    create controllers

---
+++ controllers/user-ticket.php
@@ -0,0 +1,73 @@
+<?php
+class user_ticket
+{
+    public function get($id)
+    {
+        try {
+            $response = new Response();
+            $userTicket = new UserTicketModel();
+            $result = $userTicket->get($id);
+            //Give an response
+            $response->toJSON($result);
+        } catch (Exception $e) {
+            handleException($e);
+        }
+    }
+    public function getByUserId($userId)
+    {
+        try {
+            $response = new Response();
+            $userTicket = new UserTicketModel();
+            $result = $userTicket->getByUserId($userId);
+            //Give an response
+            $response->toJSON($result);
+        } catch (Exception $e) {
+            handleException($e);
+        }
+    }
+    public function post()
+    {
+        try {
+            $request = new Request();
+            $response = new Response();
+
+            $inputJson = $request->getJSON();
+
+            // Validate required fields
+            if (!isset($inputJson->user_id) || !isset($inputJson->ticket_id) || !isset($inputJson->assigned_by)) {
+                throw new Exception("Missing required fields: user_id, ticket_id, assigned_by");
+            }
+
+            $userTicket = new UserTicketModel();
+            $result = $userTicket->insert($inputJson);
+            //Give an response
+            $response->toJSON($result);
+        } catch (Exception $e) {
+            handleException($e);
+        }
+    }
+    public function delete($id)
+    {
+        try {
+            $response = new Response();
+            $userTicket = new UserTicketModel();
+            $result = $userTicket->delete($id);
+            //Give an response
+            $response->toJSON($result);
+        } catch (Exception $e) {
+            handleException($e);
+        }
+    }
+    public function deleteByUserId($userId)
+    {
+        try {
+            $response = new Response();
+            $userTicket = new UserTicketModel();
+            $result = $userTicket->deleteByUserId($userId);
+            //Give an response
+            $response->toJSON($result);
+        } catch (Exception $e) {
+            handleException($e);
+        }
+    }
+}
